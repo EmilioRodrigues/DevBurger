d5b69a80674e26c88fb848673052f2408b8f1d46
Emilio Barbosa Rodrigues Junior <emilio.projetos@gmail.com>
2025-08-17 18:08:35 -0300

    adiciona a biblioteca uuid e atualiza a rota principal para criar um usuário

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
 // Importa apenas o Router da biblioteca express
-import { Router } from 'express';
+import { Router } from "express";
 
+import { v4 } from "uuid";
+
+import User from "./app/models/User";
 
 // Cria uma nova instância do Router, que vai gerenciar os caminhos (rotas) da aplicação
 const routes = new Router();
 
 // Define uma rota GET para o caminho principal ("/")
 // Quando alguém acessar "/", o servidor responde com um JSON contendo a mensagem "Hello World"
-routes.get("/", (request, response) => {
-  return response.status(200).json({ message: "Hello World, com js mais moderno" });
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: "Emilio",
+    email: "emilio@email.com",
+    password_hash: "erf12345qcs",
+  });
+
+  return response.status(201).json(user);
 });
 
 // Exporta as rotas para serem usadas em outros arquivos, como o app.js
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // Importa o servidor já configurado do arquivo app.js
-import app from './app'
+import app from "./app";
 
 // Faz o servidor começar a escutar na porta 3001
 // Quando o servidor estiver online, mostra uma mensagem no console
